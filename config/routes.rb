d9e27d151d587f6500f959420a9da09552b5869e
Mike Schwab <mike.schwab@gmail.com>
2020-02-15 14:59:59 -0500

    homepage set

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :contributions
   resources :benefactors
+  root :to => 'contributions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
